5559417fc6d7439c2943d8abec8a1a689494be48
Klea008 <kleakepi008@gmail.com>
2024-09-04 14:55:23 +0200

    Trying something

--- Fr/src/App.jsx
+++ Fr/src/App.jsx
@@ -1,8 +1,6 @@
 // App.jsx
 import React, { useState, createContext, useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import BookDetails from './pages/BookDetails';
 import LHome from './pages/LHome';
 
 // Create a Context for dark mode
@@ -20,9 +18,7 @@ const App = () => {
       <Router>
         <div className={`flex flex-col min-h-screen ${darkMode ? 'dark bg-gray-900 text-white' : ''}`}>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/books/:id" element={<BookDetails />} />
-            <Route path="/LHome" element={<LHome />} />
+            <Route path="/" element={<LHome />} />
           </Routes>
         </div>
       </Router>
